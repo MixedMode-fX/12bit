261aa09106dd0c88200e6951c433309b65eeecf7
Adrien Fauconnet <adrien.fauconnet@focusrite.com>
2020-05-07 23:56:23 +0100

    codec executed using IntervalTimer at known sample rate

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,21 @@
 #define CS_ADC 9
 
 #define N_CHANNELS 2
+#define SAMPLE_PERIOD 20
+
+IntervalTimer audioTimer;
+void codec();
 
 void setup(){
     spiBegin();
+    pinMode(CS_DAC, OUTPUT);    pinMode(CS_ADC, OUTPUT);
+    audioTimer.begin(codec, SAMPLE_PERIOD);
+    audioTimer.priority(0);
 }
 
-void loop(){
+void loop(){}
+
+void codec(){
     for(uint8_t i=0; i<N_CHANNELS; i++){
         dac(adc(i, CS_ADC), i, CS_DAC);
     }
